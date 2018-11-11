b136b0eb03f5f9a3ce457b6ae37eed3d21a556c6
Dimasik <daniel.protsak@gmail.com>
2018-11-12 00:49:48 +0200

    first commit

---
+++ src/main/java/homeworkFrame/App.java
@@ -0,0 +1,15 @@
+package homeworkFrame;
+
+/**
+ * Hello world!
+ *
+ */
+ import homework2.MyFileManager;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        new MyFileManager();
+    }
+}
